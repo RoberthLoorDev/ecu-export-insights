24cc495b9b814444196a7f986b588bc2a3f0884a
RoberthLoorDev <dayvidlife@gmail.com>
2024-08-27 21:43:34 -0500

    add: card autor user

--- public/images/images.tsx
+++ public/images/images.tsx
@@ -1,7 +1,17 @@
 import iconNavbar from "./icon-navbar.png";
 import barsNavbar from "./bars-navbar.png";
 import iconPresentation from "./icon-presentation.png";
-import iconProduct from "./icon-fish.png"
-import iconCategory from "./icon-category.png"
+import iconProduct from "./icon-fish.png";
+import iconCategory from "./icon-category.png";
+import me from "./me.png";
+import arrow from "./icon-arrow.png";
 
-export const images = { iconNavbar, barsNavbar, iconPresentation, iconCategory, iconProduct };
+export const images = {
+  iconNavbar,
+  barsNavbar,
+  iconPresentation,
+  iconCategory,
+  iconProduct,
+  me,
+  arrow,
+};
--- src/config/navbarOptions.tsx
+++ src/config/navbarOptions.tsx
@@ -1,19 +1,19 @@
-import { images } from "../../public/images/images"
+import { images } from "../../public/images/images";
 
 export const optionsNavbar = [
-    {
-        image: images.iconPresentation,
-        title: "Descripción General",
-        href: "/"
-    },
-    {
-        image: images.iconProduct,
-        title: "Productos",
-        href: "/"
-    },
-        {
-        image: images.iconCategory,
-        title: "Categorias",
-        href: "/"
-    }
-]
+  {
+    image: images.iconPresentation,
+    title: "Descripción General",
+    href: "/",
+  },
+  {
+    image: images.iconProduct,
+    title: "Productos",
+    href: "/",
+  },
+  {
+    image: images.iconCategory,
+    title: "Categorias",
+    href: "/",
+  },
+];
