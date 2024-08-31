a5ecbebe031f8ce0920b0eab393a360344641550
RoberthLoorDev <dayvidlife@gmail.com>
2024-08-30 20:18:57 -0500

    add: most exported category

--- src/utils/resumeUtils.ts
+++ src/utils/resumeUtils.ts
@@ -78,3 +78,68 @@ export const getTotalUniqueItems = (actualMont: string) => {
     percentageUniqueItems,
   };
 };
+
+//main export category
+interface CategoryTotals {
+  [key: string]: number;
+}
+
+export const getMainExportCategory = (actualMonth: string) => {
+  //current month
+  const categoryTotalsActualMonth = data
+    .filter((item) => item.month === actualMonth)
+    .reduce<CategoryTotals>((acc, item) => {
+      const category = item.MainProdN4.PPN4BCEDescrip;
+      const fobValue = parseFloat(item.SumOfFOBThousands);
+
+      if (!acc[category]) {
+        acc[category] = 0;
+      }
+
+      acc[category] += fobValue;
+      return acc;
+    }, {});
+
+  let mostExportedCategoryActualMonth = null;
+  let maxFOBValueActualMonth = 0;
+  for (const [category, totalFOB] of Object.entries(
+    categoryTotalsActualMonth,
+  )) {
+    if (totalFOB > maxFOBValueActualMonth) {
+      mostExportedCategoryActualMonth = category;
+      maxFOBValueActualMonth = totalFOB;
+    }
+  }
+
+  //past month
+  const pastMonth = "02";
+  const categoryTotalsPastMonth = data
+    .filter((item) => item.month === pastMonth)
+    .reduce<CategoryTotals>((acc, item) => {
+      const category = item.MainProdN4.PPN4BCEDescrip;
+      const fobValue = parseFloat(item.SumOfFOBThousands);
+
+      if (!acc[category]) {
+        acc[category] = 0;
+      }
+
+      acc[category] += fobValue;
+      return acc;
+    }, {});
+
+  let mostExportedCategoryPastMonth = null;
+  let maxFOBValuePastMonth = 0;
+  for (const [category, totalFOB] of Object.entries(categoryTotalsPastMonth)) {
+    if (totalFOB > maxFOBValuePastMonth) {
+      mostExportedCategoryPastMonth = category;
+      maxFOBValuePastMonth = totalFOB;
+    }
+  }
+
+  console.log(mostExportedCategoryActualMonth, mostExportedCategoryPastMonth);
+
+  return {
+    mostExportedCategoryPastMonth,
+    mostExportedCategoryActualMonth,
+  };
+};
